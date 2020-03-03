1573c09afdcc3d8b0f67839ff3e144beeeba052b
naunauyoh <arnaudyoh@gmail.com>
2020-03-03 16:12:14 +0100

    Skeleton for experiments done

--- experiment.py
+++ experiment.py
@@ -1,5 +1,65 @@
+"""Classes for defining our training environments"""
 
 import numpy as np
-import sklearn
-import pytorch
 
+
+class SimpleDemo:
+    """Basic Bimodal Distribution problem class"""
+
+    def __init__(self):
+        self._theta1 = 0
+        self._theta2 = 1
+        self.sigx = 2
+
+    def get_data(self, n_samples=100):
+        r"""
+        Gets n data points from a bimodal distribution
+
+        The distribution is defined as:
+        .. math::
+            \frac{1}{2} N(\theta_1, \sigma^2_x) +
+            \frac{1}{2} N(\theta_1 + \thetat_2, \sigma_x^2)
+
+        Args:
+            n_samples (int): Number of samples
+
+        Returns:
+            list: list of the sampled floats
+        """
+
+        data = np.zeros(n_samples)
+
+        for i in range(n_samples):
+            data[i] = 1/2 * np.random.normal(self._theta1, self.sigx) + \
+                      1/2*np.random.normal(self._theta1, self.sigx)
+
+        return data
+
+
+class LogReg:
+    """Logistic Regression problem class"""
+
+    def __init__(self):
+        self._theta1 = 0
+        self._theta2 = 1
+        self.sigx = 2
+
+    def get_data(self, n_samples=100):
+        r"""
+        Gets n data points from the a9a dataset (Lin. et al, 2008)
+        """
+        pass
+
+class ICA:
+    """Independent Components Analysis problem class"""
+
+    def __init__(self):
+        self._theta1 = 0
+        self._theta2 = 1
+        self.sigx = 2
+
+    def get_data(self, n_samples=1000):
+        r"""
+        Gets n data points from the a9a dataset (Lin. et al, 2008)
+        """
+        pass
